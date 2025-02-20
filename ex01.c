f5ae9b229e406f4ec99697b5f63a597eebd85fc2
Letícia <102710369+leticiaqueirozd@users.noreply.github.com>
2025-02-20 17:24:12 +0000

    simulacao SCM

---
+++ ex01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void soma(int a, int b) {
+    printf("%d + %d = %d\n", a, b, a + b);
+}
+
+void multiplica(int a, int b) {
+    printf("%d x %d = %d\n", a, b, a * b);
+}
+
+void subtrai(int a, int b) {
+    printf("%d - %d = %d\n", a, b, a - b);
+}
+
+int main() {
+    printf("Olá, Estudante!\n");
+    soma(5, 3);
+    multiplica(5, 3);
+    subtrai(5, 3);
+    return 0;
+}
